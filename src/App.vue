3c984ab10526956c37034fa6c6165afa935f0faf
Julie P <paupertjulie@gmail.com>
2020-06-18 18:04:56 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,120 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <main id="app" @mousemove="mousemove">
+
+    <section class="products">
+
+      <Product 
+        v-for="product in products"
+        :key="product.color"
+        :product="product"
+      />
+
+    </section>
+
+  </main>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from './components/Product.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Product
+  },
+  data() {
+    return {
+      products: [
+        {
+          title: "Bougie Vegan - Slow North",
+          desc: "Geranium + Rose",
+          color: "terracotta",
+          bgtxt: "Bonne",
+          src: require("./assets/candle3.jpg")
+        },
+        {
+          title: "Bougie Vegan - Slow North",
+          desc: "Eucalyptus + Lavande",
+          color: "beige",
+          bgtxt: "Fête",
+          src: require("./assets/candle2.jpg")
+        },
+        {
+          title: "Bougie Vegan - Slow North",
+          desc: "Cigtronnelle + Mandarine",
+          color: "blue",
+          bgtxt: "Maman.",
+          src: require("./assets/candle1.jpg")
+        }
+      ]
+    }
+  }, 
+  methods: {
+    mousemove (e) {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let products = document.querySelectorAll('.products .product');
+
+      for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+
+        let product_image = product.querySelector('.product-img-wrap');
+
+        let img_x = mouseX - this.coords(product_image).x;
+        let img_y = mouseY - this.coords(product_image).y;
+
+        product_image.style.transform = `translateY(-${img_y/45}px) translateX(-${img_x/45}px)`;
+
+        let bgText = product.querySelector('.bg-text');
+        let bg_x = mouseX - this.coords(bgText).x;
+        let bg_y = mouseY - this.coords(bgText).y;
+        bgText.style.transform = `translateX(${bg_x/25}px) translateY(${bg_y/25}px)`;
+      
+      }
+    },
+    coords (element) {
+      let coords = element.getBoundingClientRect();
+
+      return {
+        x: coords.left / 2,
+        y: coords.top / 2
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Helvetica Neue", sans-serif;
+  }
+
+  main {
+    width: 100vw;
+    min-height: 100vh;
+    /* overflow: hidden; */
+    background-color: #eee;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .products {
+    display: flex;
+    max-width: 1280px;
+    padding: 25px;
+    margin: 0 auto;
+  }
+
+
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,118 @@
+<template>
+
+    <div class="product">
+
+        <div :class="'product-inner ' + product.color">
+
+            <div class="product-text-wrap">
+
+                <h2 class="bg-text">{{ product.bgtxt }}</h2>
+
+            </div>
+
+            <div class="product-img-wrap">
+
+                <img :src="product.src" class="img" />
+
+            </div>
+
+            <div :class="'product-detail ' + product.color">
+
+                <h2>{{ product.title }}</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis est minima omnis dolore tempora animi veritatis? Totam deserunt distinctio eligendi?</p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+      name: "product",
+      props: ['product']
+    }
+</script>
+
+<style>
+
+    .product {
+        flex: 1 1 33.333%;
+        width: 100%;
+        padding: 25px;
+    }
+
+    .product-inner {
+        position: relative;
+        padding: 25px;
+        box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    }
+
+    .product-inner.terracotta {
+        background-image: linear-gradient(to bottom right, #dcdde1, #ffcccc);
+    }
+
+    .product-inner.beige {
+        background-image: linear-gradient(to bottom left, #dcdde1, #ced6e0 70%);
+    }
+
+    .product-inner.blue {
+        background-image: linear-gradient(to bottom right, #dcdde1, #4b6584);
+    }
+
+    .product-text-wrap {
+        position: absolute;
+        margin-left: 1rem;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 1;
+        overflow: hidden;
+    }
+
+    .product-text-wrap h2 {
+        color: #2f3542;
+        font-size: 70px;
+        font-weight: 800;
+        opacity: 0.2;
+        transform-origin: center;
+        filter: drop-shadow(0px 0px 12px rgba(0, 0, 0, 0.25));
+    }
+
+    .product-img-wrap {
+        transform-origin: center;
+    }
+
+    .product-img-wrap .img {
+        width: 100%;
+        box-shadow:
+        0 12.5px 10px rgba(0, 0, 0, 0.038),
+        0 100px 80px rgba(0, 0, 0, 0.07);
+    }
+
+    .product-detail {
+        color: #57606f;
+        opacity: 0.8;
+    }
+
+    .product-detail h2 {
+        margin-top: 1rem;
+        font-size: 18px;
+        letter-spacing: 1px;
+        font-weight: 800;
+        margin-bottom: 15px;
+    }
+
+    .product-detail p {
+        color: #2f3542;
+        margin-top: 1rem;
+        letter-spacing: 1px;
+        line-height: 1.5;
+        font-weight: 300;
+    }
+
+
+</style>
